5fc57a5ee5acfbb696ee02d7d00f6a3edfb6e5b0
Joao Vitor Alves Morais <joao.morais@venturus.org.br>
2023-01-21 15:02:20 -0300

    Criando seeds e rotas

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,24 +1,13 @@
 import Fastify from "fastify";
 import cors from "@fastify/cors";
-import { PrismaClient } from "@prisma/client";
+import { prisma } from "./lib/prisma";
+import { appRoutes } from "./routes";
 
 const app = Fastify();
-const prisma = new PrismaClient();
 
-app.register(cors, {
-  origin: ["http://localhost:3000", "http://localhost:3001"],
-});
+app.register(cors);
 
-app.get("/", async () => {
-  const habits = await prisma.habit.findMany({
-    where: {
-      title: {
-        startsWith: "Beber",
-      },
-    },
-  });
-  return habits;
-});
+app.register(appRoutes);
 
 app.listen({
   port: 3333,
