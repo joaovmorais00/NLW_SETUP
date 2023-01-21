8026f229d35b14ca63d61b0b765d4da8d9971376
Joao Vitor Alves Morais <joao.morais@venturus.org.br>
2023-01-21 16:37:22 -0300

    Starting the first home page components

---
+++ web/src/components/HabitDay.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function HabitDay() {
+  return (
+    <div className="w-10 h-10 bg-zinc-900 border border-zinc-800 rounded-lg"></div>
+  );
+}
+
+export default HabitDay;
---
+++ web/src/components/SummaryTable.tsx
@@ -0,0 +1,45 @@
+import { generateDatesFromYearBeginning } from "../utils/generate-dates-from-year-beginning";
+import HabitDay from "./HabitDay";
+
+const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"];
+
+const summaryDates = generateDatesFromYearBeginning();
+
+const minimumSummaryDatesSize = 18 * 7;
+const ammountOfDaysToFill = minimumSummaryDatesSize - summaryDates.length;
+
+function SummaryTable() {
+  return (
+    <div className="w-full flex">
+      <div className="grid grid-rows-7 grid-flow-row gap-3">
+        {weekDays.map((weekDay, index) => {
+          return (
+            <div
+              key={`${weekDay}-${index}`}
+              className="text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center"
+            >
+              {weekDay}
+            </div>
+          );
+        })}
+      </div>
+      <div className="grid grid-rows-7 grid-flow-col gap-3">
+        {summaryDates.map((date) => {
+          return <HabitDay key={date.toString()} />;
+        })}
+
+        {ammountOfDaysToFill > 0 &&
+          Array.from({ length: ammountOfDaysToFill }).map((_, index) => {
+            return (
+              <div
+                key={index}
+                className="w-10 h-10 bg-zinc-900 border border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"
+              />
+            );
+          })}
+      </div>
+    </div>
+  );
+}
+
+export default SummaryTable;
